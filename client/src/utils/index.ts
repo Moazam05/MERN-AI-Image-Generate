14b6da2582b81712c3dc03f622aa8bc2dda640e2
Moazam05 <salmanmoazam08@gmail.com>
2024-02-10 20:15:36 +0500

    reusable components

---
+++ client/src/utils/index.ts
@@ -0,0 +1,17 @@
+// File Saver Import
+import FileSaver from "file-saver";
+// Custom
+import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt: string) {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
+
+export async function downloadImage(_id: string, photo: string) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`);
+}
